ca59d5eb8459f08d61cc6bbb64d6fa244d9bb490
Long Nguyen <anonyhost@gmail.com>
2021-01-31 13:52:55 +0700

    fix enviroment variables

--- src/app/constant/called-service.const.ts
+++ src/app/constant/called-service.const.ts
@@ -1,4 +1,6 @@
-export const host = 'http://localhost:8080';
+import {environment} from '../../environments/environment';
+
+export const host = environment.apiUrl;
 export const externalContext = '/external';
 export const addTemplateEndpoint = '/template/add';
 export const getAllTemplateEndpoint = '/template';
